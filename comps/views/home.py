a453942a8485206fdbc09d16c835569cb727867d
Dan Paulson <danpaulson@gmail.com>
2021-10-20 13:01:11 -0500

    Add base empty template for homepage

---
+++ comps/views/home.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView
+
+
+class Home(TemplateView):
+    template_name = 'comps/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+
+        return context

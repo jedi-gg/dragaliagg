8b724070fc81deb5cf8e89e48bcfa37c38beeddb
Dan Paulson <danpaulson@gmail.com>
2021-10-21 20:48:55 -0500

    Clean up quest listing a bit

--- comps/views/quest_list.py
+++ comps/views/quest_list.py
@@ -1,16 +1,17 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic.list import ListView
 
-from comps.models import CompQuest
+from comps.models import Comp, CompQuest
 
 
 class CompQuestList(ListView):
     template_name = 'comps/quest.html'
     slug_url_kwarg = 'quest_slug'
+    context_object_name = 'comps'
 
     def get_queryset(self):
         self.quest = get_object_or_404(CompQuest, slug=self.kwargs['quest_slug'])
-        return CompQuest.objects.filter(slug=self.kwargs['quest_slug'])
+        return Comp.objects.filter(quest=self.quest)
 
     def get_context_data(self, **kwargs):
         context = super(CompQuestList, self).get_context_data(**kwargs)

a76abe6a3c736a9c01ce141a04c4f255277252e8
Dan Paulson <danpaulson@gmail.com>
2021-10-25 16:09:54 -0500

    Update homepage

--- comps/views/home.py
+++ comps/views/home.py
@@ -1,10 +1,14 @@
 from django.views.generic import TemplateView
 
+from comps.models import Comp
+
 
 class Home(TemplateView):
     template_name = 'comps/home.html'
 
     def get_context_data(self, **kwargs):
         context = super(Home, self).get_context_data(**kwargs)
+
+        context['comps'] = Comp.objects.all().order_by('-post_date')
 
         return context

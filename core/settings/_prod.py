e6c039439f7b7910e3ff2386ce4404ace5582fec
Dan Paulson <danpaulson@gmail.com>
2021-11-08 16:05:06 -0600

    Add manifeststatic

--- core/settings/_prod.py
+++ core/settings/_prod.py
@@ -8,3 +8,5 @@ STATIC_ROOT = os.path.join(settings.BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(settings.BASE_DIR, "_static"),
 ]
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'

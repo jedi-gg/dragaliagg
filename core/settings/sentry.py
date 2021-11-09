fe981819f755f614d64442272baaa49819dfc407
Dan Paulson <danpaulson@gmail.com>
2021-11-09 10:11:03 -0600

    Hard code dsn init

--- core/settings/sentry.py
+++ core/settings/sentry.py
@@ -1,7 +1,10 @@
+from decouple import config
+
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 sentry_sdk.init(
+    dsn=config('SENTRY_DSN'),
     integrations=[DjangoIntegration()],
     traces_sample_rate=0,
     send_default_pii=True

02eb123b261fd9bf07a5ed98997f981170c69e70
Dan Paulson <danpaulson@gmail.com>
2021-11-09 10:14:13 -0600

    Make dsn a string

--- core/settings/sentry.py
+++ core/settings/sentry.py
@@ -4,7 +4,7 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 sentry_sdk.init(
-    dsn=config('SENTRY_DSN'),
+    dsn="{}".format(config('SENTRY_DSN')),
     integrations=[DjangoIntegration()],
     traces_sample_rate=0,
     send_default_pii=True

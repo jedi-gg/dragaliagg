40a16252598726e45baefa942888239d7ccbb1f4
Dan Paulson <danpaulson@gmail.com>
2021-11-12 13:46:38 -0600

    Clear cache on deploy

--- _docker/django/entrypoint.sh
+++ _docker/django/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python manage.py collectstatic --noinput  # Collect static files
 python manage.py migrate
+python manage.py cache-clear
 
 exec gunicorn core.wsgi:application \
     --config /srv/app/_docker/django/gunicorn.py

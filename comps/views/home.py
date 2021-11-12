35fed569f9596e84336c2c02557143c4a580a920
Dan Paulson <danpaulson@gmail.com>
2021-11-12 14:14:11 -0600

    Add some basic homepage styling

--- comps/views/home.py
+++ comps/views/home.py
@@ -1,7 +1,10 @@
+from datetime import datetime, timedelta
+
+
 from django.views.decorators.cache import cache_page
 from django.views.generic.list import ListView
 
-from comps.models import Comp
+from comps.models import Comp, CompQuest
 from core.mixins import CacheMixin
 
 
@@ -12,3 +15,13 @@ class Home(CacheMixin, ListView):
 
     def get_queryset(self):
         return Comp.objects.all().order_by('-created_date')
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+
+        last_month = datetime.today() - timedelta(days=30)
+
+        # for comps in Comp.objects.filter(created_date__gte=last_month):
+        #     print(comps)
+
+        return context

c411897d1916b60059fd087d853429c1644b8351
Dan Paulson <danpaulson@gmail.com>
2021-11-12 13:44:08 -0600

    Migrate on entrypoint

--- _docker/django/entrypoint.sh
+++ _docker/django/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python manage.py collectstatic --noinput  # Collect static files
-python manage.py compress
+python manage.py migrate
 
 exec gunicorn core.wsgi:application \
     --config /srv/app/_docker/django/gunicorn.py

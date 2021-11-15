18fd98ecca948a7eb146a48f8f5d87a940048ac1
Dan Paulson <danpaulson@gmail.com>
2021-11-15 14:24:15 -0600

    Update entrypoint for local use

--- _docker/django/entrypoint.sh
+++ _docker/django/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-python manage.py collectstatic --noinput  # Collect static files
+pip-sync
 python manage.py migrate
-python manage.py cache-clear
 
-exec gunicorn core.wsgi:application \
-    --config /srv/app/_docker/django/gunicorn.py
+if [ "$ENV" == "PROD" ]; then
+    python manage.py collectstatic --noinput  # Collect static files
+    python manage.py cache-clear
+    exec gunicorn core.wsgi:application \
+        --config /srv/app/_docker/django/gunicorn.py
+fi
+
+if [ "$ENV" == "DEV" ]; then
+    exec python manage.py runserver 0.0.0.0:8000
+fi
